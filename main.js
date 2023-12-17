f5a0051a7c2c28d52b8e7395813ce3419bf8f757
fuad005-cyber <fuadbores@gmail.com>
2023-12-17 23:15:55 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,166 @@
+// TASK 1 --> Konsola mesaj verin: «My name is Murad».
+// console.log("My name is Murad")
+
+
+// TASK 2 --> Adı name və adınızın mənimsədildiyi sabit dəyişən təyin edin.Bu dəyişənin dəyərini konsolda göstərin.
+// let userName = 'Fuad'
+// console.log(userName)
+
+
+// TASK 3 --> Bu adlardan hansının dəyişən üçün istifadə oluna biləcəyini göstərin.Birdən çox variant seçin.
+// person, console, $add
+
+
+// TASK 4 --> Depodaki bütün kitabların ümumi dəyərini konsola çıxarın Kitab mağazasındaki kitabın qiyməti bookPrice dəyişənindəvə depodaki kitab sayı bookAmount dəyişənində saxlanılır.Depodaki bütün kitabların ümumi dəyərini konsola çıxarın.
+// let bookPrice = 750;
+// let bookAmount = 14;
+// let allPrice = bookPrice * bookAmount
+// console.log(allPrice)
+
+
+// TASK 5 --> Dəyişən dəyərini saniyə metrə çevirin və onu konsola çıxarın.kmhSpeed ​​dəyişəni avtomobilin sürətinin «saatda kilometrlərlə» dəyərini saxlayır. msSpeed ​​dəyişəni təyin edin və ona eyni sürət dəyərini «saniyədə metrə» çevirərək yazın.msSpeed dəyişəninin dəyərini konsola çıxarın.
+// let kmhSpeed = 75;
+// let msSpeed = kmhSpeed * 1000 / 3600
+// console.log(msSpeed)
+
+
+// TASK 6 --> İstifadəçinin doğum tarixini soruşan və bu ə qədər ğı günlərin sayını göstərən proqram yazın.
+// let birthDate = prompt("doğum tarixini daxil edin:")
+// let toDay = alert("Siz " + (2023 - birthDate) * 365 + " gün yaşadıniz") 
+
+
+// TASK 7 --> Ədədlər üzərində əməliyyatları yerinə yetirin. num dəyişənində iki rəqəmli ədəd saxlanılır.Konsola çıxaran belə proqram yazın:- Bu ədədin beşinci dərəcə qüvvətə qaldırılmış vəziyyətini.- 3-ə bölünmədən qalan bu ədədin qalığını.
+// let num = 5
+// let numberDegree = num ** 5
+// let numDivide = 5%3
+// console.log(numberDegree, numDivide)
+
+
+// TASK 8 --> Konsola bu növ mesaj çıxarın “Sizin adınız [firstName] [lastName].Tanışlığımıza çox şadam!”firstName dəyişənində istifadəçinin adı,lastName — dəyişənində isə soyadı saxlanır.Konsola bu növ mesaj çıxarın “Sizin adınız [firstName] [lastName]. Tanışlığımıza çox şadam!”=>let firstName = 'Alan';=>let lastName = 'Turing';
+// let firstName = prompt("Adınızı daxil edin")
+// let lastName = prompt(" Soyadınızı daxil edin")
+// console.log("Sizin adınız " + firstName + lastName + " Tanışlığımıza çox şadam!")
+
+
+// TASK 9 --> Şablon mətnlərdən istifadə edərək,konsola “Sizi bir daha görməyimizə şadıq, [firstName] [lastName]”.firstName dəyişənində istifadəçinin adı, lastName — dəyişənində isə soyadı saxlanır.
+// let firstName = prompt("Adınızı daxil edin")
+// let lastName = prompt(" Soyadınızı daxil edin ")
+// console.log("Sizin bir daha görməyimizə şadıq " + firstName + lastName )
+
+// TASK 10 -->Addaki hərflərin sayını konsola çıxarın.fullName dəyişənində istifadəçinin tam adı saxlanılır. Addaki hərflərin sayını konsola çıxarın.Probel hərf olaraq sayılmır.let fullName = "Murad Orucov"
+// let fullName = "Murad Orucov"
+// console.log(fullName.length)
+
+
+
+
+// TASK 11 --> İstifadəçinin ad soyadının baş hərflərini konsola çıxaran proqram yazın.İstifadəçinin adını və soyadını boşluqlarla ayıraraq fullName dəyişəninə yazın.fullName dəyişəninin dəyərindən istifadə edərək istifadəçinin baş hərflərini konsola çıxaran proqram yazın.
+
+
+// !!!!!!!
+
+// TASK 12 --> İstifadəçidən tam adını daxil etməyini istəyin və soyadı ilə adının yerini dəyişin.İstifadəçidən tam adı «AD SOYAD» formatında daxil etməyi xahiş edin.Tam adını konsola fərqli qaydada yəni «SOYAD AD» formatında çıxarın,
+// let userName = prompt("Adinizi daxil edin")
+// let userSurname = prompt("Soyadinizi daxil edin")
+// console.log(userSurname + userName)
+
+
+// TASK 13 --> Konsola "Salam, [ad]!" formatında salamlama mesajı çıxarın.İstifadəçidən adını daxil etməsini istəyin.Konsola "Salam, [ad]!" formatında salamlama mesajı çıxarın.
+// let userName = prompt("Adinizi daxil edin")
+// console.log("Salam, " + userName + "!")
+
+
+// TASK 14 --> İki ədədin toplama, çıxma, vurma və bölməsinin nəticələrin konsola alt-alta 4 sətir olaraq çıxarın.İstifadəçidən iki ayrı prompt əmri ilə iki ədəd daxil etməsini istəyin.Bu ədədlərin toplama, çıxma, vurma və bölməsinin nəticələrin konsolaa lt-alta 4 sətir olaraq çıxarın.
+// let user1 = prompt("Birinci ededi daxil edin")
+// let user2 = prompt("Ikinci ededi daxil edin")
+// console.log(user1 + user2)
+// console.log(user1 - user2)
+// console.log(user1 * user2)
+// console.log(user1 / user2)
+
+
+// TASK 15 --> 12345 və 98765 ədədlərinin hasilindən alınan nəticənin 6-cı rəqəmini tapan proqram yazın   !!!!
+// let num1 = 12345
+// let num2 = 98765
+// console.log((num1 * num2).toLocaleString[6])
+
+
+// TASK 16 --> Əməliyyatların nəticəsini müəyyənləşdirin.
+// 1) 10, 2)-10 , 3) NaN , 4) 2 , 5) 9px , 6)  $45 , 7) 2 , 8) NaNpx , 9)  infinity , 10) -95 , 11) -14, 12) 1 , 13) NaN
+
+
+// TASK 17 --> Rəqəmlərin cəmini göstərən proqram yazın.İstifadəçidən dörd rəqəmli ədəd daxil etməsini xahiş edən və sonra daxil edilmiş ədədlərin rəqəmlərinin cəmini konsola çıxaran proqram yazın.
+
+
+
+// TASK 18 -->  İstifadəçinin soyadını və adını almaq üçün proqram tərtib edin.Proqram bir mesajla istifadəçinin soyadını və adını daxil etməsini istəyir və konsola «Salam, <ad>!» mesajını çıxarır.
+// let userInfo = prompt("Adinizi ve Soyadinizi daxil edin:");
+// let [userName, userSurname] = userInfo.split(" ");
+// console.log("Salam, " + userName + "!");
+
+
+// TASK 19 --> Qiyməti mətn halına salan proqram tərtib edin.İstifadəçidən «530.90» formatında qiymət daxil etməyi xahiş edən və qiyməti «530 manat 90 qəpik» formatında konsola çıxaran proqram tərtib edin.
+// let num = prompt(" Xahis edirik 530.90 qiymetini daxil edesiniz")
+// let num1 = "530 manat "
+// let num2 = " 90 qepik"
+// console.log(num1 + num2)
+
+
+// TASK 20 --> Vergi nəzərə alınmadan əmək haqqı məbləğini hesablayan proqram tərtib edin.Proqram istifadəçidən əmək haqqı məbləğin soruşur və 13% vergi istisna olmaqla məbləği konsola çıxarır.
+// let payMent = prompt("əmək haqqı məbləğin daxil edin: ")
+// let tax = (payMent * 13 ) / 100 
+// console.log(tax)
+
+
+// TASK 21 --> İstifadəçidən soyad, ad və ata adını istəyən, soyadı və baş hərfləri konsola çıxaran proqram tərtib edin. Məsələn, «İvanov Pyotr Sergeevich» — «İvanov P.S.».
+
+
+// TASK 22 --> Depozitdən mənfəətin hesablanması üçün proqram yazın.Proqram istifadəçidən əmanətin məbləğini, müddətini və faiz dərəcəsini daxil etməsini istəyir və gözlənilən mənfəəti hesablayır.
+
+// let num = prompt("əmanətin məbləğini daxil edin")
+// let period = prompt("nece illik oldugunu daxil edin")
+// let percent = prompt("faiz dərəcəsini daxil edin")
+// let all = ((num * percent) / 100 ) * period
+// console.log(all)
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
